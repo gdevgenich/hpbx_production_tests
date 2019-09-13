f5916fce3f18377690d0f224a0ab04dc755eaf01
dgirdyuk <dgirdyuk@intermedia.net>
2019-09-13 04:55:17 -0700

    Add configuration for hpbx2

---
+++ tests/HPBX_Next_Gen/Production_tests/Smoke_tests/NG_18116.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from step_manager.pbxut import StepTestCase
+from call_functions import OrderedCall
+from call_functions.matchers.matchers import smart_compare
+from context import Context
+
+
+class NG_113(StepTestCase):
+    """
+    @name: NG-113
+    @summary: FMFM_sim_two_internal_users_ext_ext
+    @suite: HPBX_Next_Gen.Automation_functional_tests.FMFM_sim.call_from_external_user
+    @version: 1
+    """
+
+    def setUp(self):
+        self.context = Context.instance()
+        self.external_user3 = self.context.get('external_user3')
+        self.user2 = self.context.get('user1')
+        self.user3 = self.context.get('user2')
+        self.user4 = self.context.get('user3')
+        self.user2.remove_all_fmfm()
+        self.user2.add_fmfm(number=self.user3.get_extension(), order=50, timeout=10)
+        self.user2.add_fmfm(number=self.user4.get_extension(), order=50, timeout=10)
+
+        self.devices = [
+            self.user2.get_sipre_client(), self.user3.get_sipre_client(),
+            self.user4.get_sipre_client()]
+
+        self.order = [[0], [1, 2]]
+
+        self.timeouts = [10, 6, 2]
+
+    def initialize(self, sm):
+        # build execute info
+
+        execute_info = {
+            "bob": self.external_user3.get_sipre_client(),
+            "alice": self.user4.get_sipre_client(),
+            "call_to": self.external_user3.get_sip_uri(self.user2.get_phone_number()),
+            "devices": self.devices,
+            "order": self.order,
+            "timeouts": self.timeouts,
+            "sm": sm
+        }
+
+        OrderedCall(**execute_info)
+
+        # check call history
+        call_1 = {'from': self.external_user3.get_phone_number(), 'caller_name': self.external_user3.get_cnam(),
+                  'to': self.user2.get_phone_number(), 'called_name': self.user2.get_display_name()}
+
+        sm.add_step("Check call history").add_expected(
+             self.user2.get_account().check_call_history, calls=[call_1])
+
+    def tearDown(self):
+        self.user2.remove_all_fmfm()
