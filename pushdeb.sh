b30e3e4db781f2df826e76a9c3c7e16046a96f5d
Mahdi Ardekanian <mardekanian@intermedia.net>
2022-08-23 15:44:11 +0000

    HPBX-10628 adds bullseye support

--- pushdeb.sh
+++ pushdeb.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-curl -X POST -F file=@"$(ls|grep 'amd64.deb')" --user ${APTUSER}:${APTPASS} ${APTURL}/files/${APT_DIR}
+curl -X POST -F file=@"$(ls|grep ""*${DEB_OS_PREFIX}_amd64.deb"")" --user ${APTUSER}:${APTPASS} ${APTURL}/files/${APT_DIR}
 curl -X POST --user ${APTUSER}:${APTPASS} ${APTURL}/repos/${APT_LOCAL_REPO}/file/${APT_DIR}
 curl -X PUT -H 'Content-Type:application/json' \
 --data '{"Sources":[{"Component":"non-free"}], "Architectures":["amd64"], "Distribution":"${DISTR}"}' \
